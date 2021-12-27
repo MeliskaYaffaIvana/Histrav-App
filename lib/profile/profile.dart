b8bbe753a8943bf36dbe8cbfe635c4faecfb5bef
lailaaliefrasuliana <75661008+lailaaliefrasuliana@users.noreply.github.com>
2021-12-27 12:53:01 +0700

    profile page

---
+++ lib/profile/profile.dart
@@ -0,0 +1,83 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:histrav_app_flutter/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'package:histrav_app_flutter/Screens/login_screen.dart';
+
+class ProfileApp extends StatefulWidget {
+  const ProfileApp({Key? key}) : super(key: key);
+
+  @override
+  _ProfileAppState createState() => _ProfileAppState();
+}
+
+class _ProfileAppState extends State<ProfileApp> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffCEEEDD),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 150,
+              child: Image.asset("assets/avatar.jpg", fit: BoxFit.contain),
+            ),
+            const Text(
+              "Welcome Back",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text("${loggedInUser.firstName} ${loggedInUser.secondName}",
+                style: const TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w500,
+                )),
+            Text("${loggedInUser.email}",
+                style: const TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w500,
+                )),
+            const SizedBox(
+              height: 15,
+            ),
+            ActionChip(
+                backgroundColor: const Color(0xff8FC88E),
+                label: const Text("Logout"),
+                onPressed: () {
+                  logout(context);
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// the logout function
+Future<void> logout(BuildContext context) async {
+  await FirebaseAuth.instance.signOut();
+  Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const LoginScreen()));
+}
